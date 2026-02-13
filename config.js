0dc5c35ee158c2ed1987785df06a3d80a4ee7584
joshuadanieleng-ui <joshua.daniel.eng@gmail.com>
2026-02-13 18:38:15 -0500

    Update config.js

--- config.js
+++ config.js
@@ -42,7 +42,7 @@ const CONFIG = {
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much, bubbaa (not as much as me though)!!!?? 🥰🚀💝",  // Shows when they go past 5000%
+        extreme: "WOOOOW You love me that much Bubba?!? (not as much as me though hehe) 🥰🚀💝",  // Shows when they go past 5000%
         high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
         normal: "And beyond! 🥰"                           // Shows when they go past 100%
     },
